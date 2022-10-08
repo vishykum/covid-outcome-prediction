1260b25c925ff43784b53107ee3e947b8444f421
unknown <ercoulte@sfu.ca>
2022-10-07 19:52:49 -0700

    Add windows open notebook batch script & hopefully fix environment file

---
+++ colab_connect_command.bat
@@ -0,0 +1,1 @@
+pip install --upgrade jupyter_http_over_ws>=0.0.7 && jupyter serverextension enable --py jupyter_http_over_ws && jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8888 --NotebookApp.port_retries=0
